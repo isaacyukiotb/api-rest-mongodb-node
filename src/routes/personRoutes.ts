804f8c1505a90217394ba058943296114b009006
Isaac Yukio <isaacyukio@gmail.com>
2022-07-11 16:25:10 -0300

    Resgatando os dados da API

--- src/routes/personRoutes.ts
+++ src/routes/personRoutes.ts
@@ -1,4 +1,5 @@
-import { Router, Request, Response,  } from 'express'
+import { Router, Request, Response } from 'express'
+
 import Person from '../models/Person'
 import PersonI from '../models/PersonInterface'
 
@@ -6,33 +7,36 @@ import PersonI from '../models/PersonInterface'
 const router = Router();
 //rotas da api
 
-router.post('/', async (req:Request, res:Response)=>{
+router.post('/', async (req: Request, res: Response) => {
 
     //req.body
-    const {name, salary, approved} = req.body
-    const person:PersonI = {name, salary, approved};
-    
+    const { name, salary, approved } = req.body
+    const person: PersonI = { name, salary, approved };
+
     // create
 
     try {
 
         await Person.create(person);
-        res.status(201).json({message:'Pessoa inserida no sistema com sucesso!'});
+        res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' });
 
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({ error: error })
     }
-    
+
 })
 
-//endPoint inicial / rota
-router.get('/', (req: Request, res: Response) => {
-    //mostrar a req
-    res.json(
-        {
-            message: 'Oi Express!'
-        }
-    )
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        
+        const people = await Person.find();
+        res.status(200).json(people);
+
+    } catch (error) {
+
+        res.status(500).json({ error: error })
+    }
 })
 
 export default router;

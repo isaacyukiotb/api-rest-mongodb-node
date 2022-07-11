1a01f943b7af07051002f0931f1984d66e71dcea
Isaac Yukio <isaacyukio@gmail.com>
2022-07-11 19:44:05 -0300

    Finalizando o CRUD da aplicacao

--- src/routes/personRoutes.ts
+++ src/routes/personRoutes.ts
@@ -7,6 +7,8 @@ import PersonI from '../models/PersonInterface'
 const router = Router();
 //rotas da api
 
+
+//criando dados
 router.post('/', async (req: Request, res: Response) => {
 
     //req.body
@@ -26,10 +28,10 @@ router.post('/', async (req: Request, res: Response) => {
 
 })
 
-
+//Leitura de Dados
 router.get('/', async (req: Request, res: Response) => {
     try {
-        
+
         const people = await Person.find();
         res.status(200).json(people);
 
@@ -37,6 +39,87 @@ router.get('/', async (req: Request, res: Response) => {
 
         res.status(500).json({ error: error })
     }
+})
+
+//Criando rotas dinamicas
+router.get('/:id', async (req: Request, res: Response) => {
+
+    //extrair o dado pela requisicao, pela URL = req.params
+    const id: string = req.params.id;
+
+    try {
+
+        const person = await Person.findOne({ _id: id })
+
+        if (!person) {
+            res.status(422).json({ message: 'O Usuario nao foi encontrado!' })
+            return
+        }
+
+        res.status(200).json(person);
+
+
+    } catch (error) {
+
+        res.status(500).json({ error: error })
+    }
+})
+
+// Update - Atualizacao de dados (PUT, PATCH) (PUT - Atualizar todos os dados) (PATCH - Atualizar apenas alguns campos)
+router.patch('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+
+    const { name, salary, approved } = req.body;
+
+    const person: PersonI = {
+        name,
+        salary,
+        approved
+    }
+
+
+    try {
+
+        const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+        //Verifica se algum dado foi modificado
+        if (updatedPerson.matchedCount === 0) {
+            res.status(422).json({ message: 'O Usuario nao foi encontrado!' })
+            return
+        }
+
+        res.status(200).json(person);
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+
+    const id = req.params.id;
+
+    const person = await Person.findOne({ _id: id })
+
+    if (!person) {
+        res.status(422).json({ message: 'O Usuario nao foi encontrado!' })
+        return
+    }
+
+    try {
+
+        await Person.deleteOne({_id:id});
+
+        res.status(200).json({message:'Usuario removido com sucesso!'});
+
+    } catch (error) {
+
+        res.status(500).json({ error: error })
+
+    }
+
+
 })
 
 export default router;
